40979ac38bbeb0d52bdc962ed4ba60b2e1214adc
Ilja Leijo <ilja.leijo@gmail.com>
2016-06-27 00:53:34 +0300

    added country-based distances into config

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 'use strict'
 
 module.exports = {
+  'distance': {
+    'de': '10',
+    'at': '20',
+    'ch': '17.5',
+    'nl': '9'
+  },
   'conString': 'postgres://testdb:testdb@localhost:5432/testdb'
 }
